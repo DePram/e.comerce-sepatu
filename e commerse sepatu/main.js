ace3109359369118890c573349f6f51f5c9516c8
DePram <165004453+DePram@users.noreply.github.com>
2024-06-25 12:22:54 +0800

    upload folder e comerce sepatu

---
+++ e commerse sepatu/main.js
@@ -0,0 +1,6 @@
+const menuToggle = document.querySelector(".biru");
+const nav = document.querySelector("content");
+
+menuToggle.addEventListener("click", function () {
+  nav.classList.toggle("gbiru");
+});
